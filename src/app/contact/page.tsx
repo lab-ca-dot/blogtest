18ccf61788d0a993cf748fe4c7349697e4f84a8c
CCC- Higuchi <higuchi@lab-ca.com>
2025-05-16 14:11:01 +0900

    change

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -5,21 +5,21 @@ import Container from "@/app/_components/container";
 export default function ContactForm() {
   return (
     <>
-    <Head>
-        <script src="https://www.google.com/recaptcha/api.js" async defer/>
+      <Head>
+        <script src="https://www.google.com/recaptcha/api.js" async defer />
       </Head>
-    <Container>
-      <form
-        action="https://ssgform.com/s/YxudAFlPwiMX"
-        method="POST"
-      >
-        <input type="text" name="name" placeholder="お名前" required />
-        <input type="email" name="email" placeholder="メールアドレス" required />
-        <textarea name="message" placeholder="お問い合わせ内容" required />
-        <button type="submit">送信</button>
-        <div className="g-recaptcha" data-sitekey="6LfyvjwrAAAAAEDQbowrLdY7cd1J-uATHDbZ9yAM"></div>
-      </form>
+      <Container>
+        <form
+          action="https://ssgform.com/s/YxudAFlPwiMX"
+          method="POST"
+        >
+          <input type="text" name="name" placeholder="お名前" required />
+          <input type="email" name="email" placeholder="メールアドレス" required />
+          <textarea name="message" placeholder="お問い合わせ内容" required />
+          <div className="g-recaptcha" data-sitekey="6LfyvjwrAAAAAEDQbowrLdY7cd1J-uATHDbZ9yAM" />
+          <button type="submit">送信</button>
+        </form>
       </Container>
-      </>
+    </>
   )
 }
