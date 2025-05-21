7399e080951899b587ad8c7bef3723f4782ca227
CCC- Higuchi <higuchi@lab-ca.com>
2025-05-22 02:41:25 +0900

    リンクの整理、フッターにお問い合わせ追加

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -1,6 +1,6 @@
 import styles from './Footer.module.scss'
 import Container from '@/app/_components/container'
-import { EXAMPLE_PATH } from '@/lib/constants'
+import Link from "next/link";
 
 export function Footer() {
   return (
@@ -13,14 +13,20 @@ export function Footer() {
           <div className={styles.actions}>
             <a
               href="https://lab-ca.com/" target='_blank'
-              className={styles.button}
+              className={styles.button} rel="noopener noreferrer"
             >Lab-Ca.公式サイト</a>
             <a
-              href='https://github.com/lab-ca-dot/nextjs-blog'
-              className={styles.link}
+              href='https://github.com/lab-ca-dot/nextjs-blog' target='_blank'
+              className={styles.link} rel="noopener noreferrer"
             >
               このブログのGithubはこちら
             </a>
+            <Link
+              href={`/contact/`}
+              className={styles.link}
+            >
+              お問い合わせはこちら
+            </Link>
           </div>
           <address className={styles.address}>&copy; Lab-Ca.</address>
         </div>
